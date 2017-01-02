7fef8ba76a4cc379e5d238430198437c4dc3b499
woh <wolfgang.hobmaier@datapweb.de>
2017-01-02 17:14:29 +0100

    Ionic Version Bump and Cleanup

--- ionic2/src/app/main.prod.ts
+++
@@ -1,9 +0,0 @@
-import { platformBrowser } from '@angular/platform-browser';
-import { enableProdMode } from '@angular/core';
-
-import { AppModuleNgFactory } from './app.module.ngfactory';
-import { registerServiceWorker } from './register-service-worker'
-
-registerServiceWorker();
-enableProdMode();
-platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
